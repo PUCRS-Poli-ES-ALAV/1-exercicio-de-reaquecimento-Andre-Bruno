c5242243b14fc14c310ea8cd2fadfd7fb338c125
bramos013 <brunoramos013@gmail.com>
2022-03-06 01:39:04 -0300

    Exercicio 1.i

--- RaqueciApp/src/main/java/br/pucrs/alav/Recursion.java
+++ RaqueciApp/src/main/java/br/pucrs/alav/Recursion.java
@@ -1,5 +1,19 @@
 package br.pucrs.alav;
 
 public class Recursion {
-	
+
+    // Multiplicação de dois números naturais, através de somas sucessivas (Ex.: 6 ∗ 4 = 4 + 4 + 4 + 4 + 4 + 4).
+    public static int somaSucessiva(int fator1, int fator2 ){
+    int produto = 0;
+        if(fator1 == 0)
+            return produto;
+        return produto = fator2 + somaSucessiva(fator1-1, fator2);   
+    }
+
+	public static void main(String[] args) {
+        int fator1 = 6;
+        int fator2 = 4;
+
+        System.out.println(somaSucessiva(fator1, fator2));
+    }
 }
